4b24fcf554b6bb80c176a79981b7b3dc1a4df67e
Jericho Jackson <jerichoj@orionhealth.com>
2023-12-28 13:05:08 +1300

    Initial commit

---
+++ test/blog-aggregator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogAggregator from '../lib/blog-aggregator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-aggregator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogAggregator.BlogAggregatorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
